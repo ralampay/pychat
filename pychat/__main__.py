da69e84af55c5915f5e01268ba7b25e976713e87
jproberto97 <jasonroberto38@gmail.com>
2022-12-19 14:32:25 +0800

    resolves #5

--- pychat/__main__.py
+++ pychat/__main__.py
@@ -24,9 +24,15 @@ def main():
             engine="text-davinci-003", prompt=query, max_tokens=4000
         )
 
-        output = completion.choices[0].text
+        if len(completion.choices) == 0:
 
-        print("Output: {}".format(output))
+            print("No output")
+        
+        else:
+
+            output = completion.choices[0].text
+
+            print("Output: {}".format(output))
 
 
 if __name__ == "__main__":
