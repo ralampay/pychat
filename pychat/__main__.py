fb20a8f99f37f016d8da48b1f1e1cd49618e0592
Raphael Alampay <raphael.alampay@gmail.com>
2022-12-19 11:10:50 +0800

    Handle the detection of OPENAI_API_KEY availability. Resolves #1

--- pychat/__main__.py
+++ pychat/__main__.py
@@ -1,6 +1,15 @@
 import openai
+import os
+
+OPENAI_API_KEY='OPENAI_API_KEY'
 
 def main():
+    openai_api_key = os.getenv(OPENAI_API_KEY)
+
+    if openai_api_key == None:
+        print("OPENAI_API_KEY required")
+        exit(-1)
+
     print("PyChat v0.1")
     query = input('Input: ')
 
