6b1981c0c2faae7aafa7bd8bfc233ce042d8ef77
Eugene <eferrancullo@77global.biz>
2022-12-19 18:38:47 +0800

    changed value of query parameter and changed to not required, changed loop to while True

--- pychat/__main__.py
+++ pychat/__main__.py
@@ -7,49 +7,49 @@ OPENAI_API_KEY = "OPENAI_API_KEY"
 
 def main():
     parser = argparse.ArgumentParser(description="PyChat v0.1")
-
+    
     parser.add_argument("--max-tokens", help="Maximum size of tokens to be used", type=int, default=4000)
-    parser.add_argument("--engine", help="The openai engine to use", type=str, default='text-davinci-003')
-    #parser.add_argument("--query",help="Checking for user input",type=str,required=True)
+    parser.add_argument("--engine", help="The openai engine to use", type=str, default="text-davinci-003")
+    parser.add_argument("--query", help="Checking for user input", type=str, default="Greet me")
+
     args = parser.parse_args()
 
     max_tokens  = args.max_tokens
     engine      = args.engine
-    #query       = args.query
-    print("Options:")
+    query       = args.query
+
+    print("\nOptions: ")
     print("max_tokens: {}".format(max_tokens))
     print("engine: {}".format(engine))
 
-    counter = 0
-    while counter != 1:
-        openai_api_key = os.getenv(OPENAI_API_KEY)
+    openai_api_key = os.getenv(OPENAI_API_KEY)
 
-        if openai_api_key == None:
-            print("OPENAI_API_KEY required")
-            exit(-1)
+    if openai_api_key == None:
+        print("OPENAI_API_KEY required")
+        exit(-1)
 
-        query = input("Enter a question or type quit to exit: ")
+    query = query.strip()
 
-        test_input = query.strip()
-
-        if test_input == '':
+    while True:
+        if query.lower() == 'quit':
+            print("Thank you for using PyChat!")
+            break
+        elif query == '':
             print("You did not enter any prompt.")
-        elif test_input == 'quit':
-            counter += 1
-            print("ok bye. Hope too see you soon")
+            query = input("Please enter a prompt: ")
         else:
             completion = openai.Completion.create(
-            engine=engine, prompt=query, max_tokens=max_tokens
+                engine="text-davinci-003", prompt=query, max_tokens=max_tokens
             )
 
             if len(completion.choices) == 0:
-
                 print("No output")
-                
             else:
                 output = completion.choices[0].text
+                print(format(output))
 
-                print("Output: {}".format(output))
+            query = input("Prompt [or type 'quit' to exit]: ")
+            query = query.strip()
 
 if __name__ == "__main__":
     main()
