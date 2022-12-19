6ccad40ae2671828876e10a1d1b1ed1112018ae1
John Nicholas Dato <jdato@77global.biz>
2022-12-19 18:10:53 +0800

    changed the user input from parametized to prompt

--- pychat/__main__.py
+++ pychat/__main__.py
@@ -10,47 +10,46 @@ def main():
 
     parser.add_argument("--max-tokens", help="Maximum size of tokens to be used", type=int, default=4000)
     parser.add_argument("--engine", help="The openai engine to use", type=str, default='text-davinci-003')
-    parser.add_argument("--query", help="Provide user input", type=str, required=True)
-
+    #parser.add_argument("--query",help="Checking for user input",type=str,required=True)
     args = parser.parse_args()
 
     max_tokens  = args.max_tokens
     engine      = args.engine
-    query       = args.query
-
+    #query       = args.query
     print("Options:")
     print("max_tokens: {}".format(max_tokens))
     print("engine: {}".format(engine))
-    #print("query: {}".format(query))
 
-    openai_api_key = os.getenv(OPENAI_API_KEY)
+    counter = 1
+    while counter != 2:
+        openai_api_key = os.getenv(OPENAI_API_KEY)
 
-    if openai_api_key == None:
-        print("OPENAI_API_KEY required")
-        exit(-1)
+        if openai_api_key == None:
+            print("OPENAI_API_KEY required")
+            exit(-1)
 
-    #query = input("Input: ")
+        query = input("Ask me a question[or type quit to exit]: ")
 
-    while query != 'quit':
         test_input = query.strip()
-        
+
         if test_input == '':
             print("You did not enter any prompt.")
+        elif test_input == 'quit':
+            counter += 1
+            print("okay bye")
         else:
             completion = openai.Completion.create(
-                engine=engine, prompt=query, max_tokens=max_tokens
+            engine=engine, prompt=query, max_tokens=max_tokens
             )
 
             if len(completion.choices) == 0:
+
                 print("No output")
+                
             else:
                 output = completion.choices[0].text
-                print("Output: {}".format(output))
 
-        print('-'*70)
-        query = input("\nInput ('quit' to exit): ")
-        
-    print('\nThank you for using PyChat!')
+                print("Output: {}".format(output))
 
 if __name__ == "__main__":
     main()
